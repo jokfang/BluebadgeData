32d156081338c6e81837a27486b5b4b66bfc7bb5
jokfang <jonathan0her@gmail.com>
2022-12-03 20:01:07 +0100

    test

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 {
-    "jokfang": "vilains",
-    "lauvel": "pirates",
-    "Jotaku__" : "super",
-    "Senua_Rhaenyra" : "vilains",
-    "melaniecassoulet" : "vilains",
-    "aiola59" : "pirates",
-    "mimilili334" : "vilains"
+    "melaniecassoulet":"vilains"
 }
