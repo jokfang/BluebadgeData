2d5740218a535fe9e6c4853fc04c0ce70bf4e529
jokfang <jonathan0her@gmail.com>
2022-12-03 19:53:36 +0100

    add

--- index.js
+++ index.js
@@ -2,5 +2,8 @@
     "jokfang": "vilains",
     "lauvel": "pirates",
     "Jotaku__" : "super",
-    "Senua_Rhaenyra" : "vilains"
+    "Senua_Rhaenyra" : "vilains",
+    "melaniecassoulet" : "vilains",
+    "aiola59" : "pirates",
+    "mimilili334" : "vilains"
 }
